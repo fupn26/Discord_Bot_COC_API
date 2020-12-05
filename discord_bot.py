9d41299e9c30bf4b35426db406b124af74721860
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-05 14:36:49 +0100

    added coc and pyjavaproperties packages

--- discord_bot.py
+++ discord_bot.py
@@ -1,11 +1,17 @@
 import discord
+import coc
 from discord.ext import commands
 from pyjavaproperties import Properties
 
 bot = commands.Bot(command_prefix="!")
+p = Properties()
+p.load(open('bot.properties'))
+
+coc_client = coc.login(p['coc_email'], p['coc_pass'], key_count=5, key_names="Bot key", client=coc.EventsClient)
 
 @bot.command()
 async def hello(ctx):
     await ctx.send("Hello!")
 
-bot.run('Nzg0NzIwNDk3NTA5NDAwNTc2.X8tZ6g.FMw0bPF4u_qRpZgDcAZ055byWdU')
+
+bot.run(p['bot_token'])
